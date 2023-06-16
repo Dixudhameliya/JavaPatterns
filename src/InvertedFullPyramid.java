f8a9da3b6402969e7f34568a1149bdea9b647513
Dixita25 <dixudhameliya@gmail.com>
2023-06-16 18:12:13 -0400

    java pattern commit.

---
+++ src/InvertedFullPyramid.java
@@ -0,0 +1,16 @@
+
+public class InvertedFullPyramid {
+
+	public static void main(String[] args) {
+		
+		for(int i=6;i>=1;i--) {
+			for(int j=i;j<=5;j++) {
+				System.out.print(" ");
+			}for(int k=1;k<=i;k++) {
+				System.out.print(" *");
+			}System.out.println();
+		}
+
+	}
+
+}
---
+++ src/NumbersPatternProgram/Pattern12.java
@@ -0,0 +1,19 @@
+package NumbersPatternProgram;
+
+public class Pattern12 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=7;i++) {
+			
+			int num=(i%2==0)?0:1;
+			System.out.print(num);
+			
+			
+		 for(int j=1;j<=7;j++) {
+				num=num==1?0:1;
+				System.out.print(num);
+		}System.out.println();
+
+	}}}
+
+
